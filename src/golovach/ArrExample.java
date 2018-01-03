8c9c85a54b8f10e0414aa191a74c6af8f74b3cac
Maxim Musienko <mmusienko@codenvy.com>
2018-01-03 16:10:26 +0200

    marege 02012018

---
+++ src/golovach/ArrExample.java
@@ -0,0 +1,11 @@
+package golovach;
+
+public class ArrExample {
+    public static void main(String[] args) {
+        Object [] objects = new Integer[2];
+        objects[0] = 1;
+        objects[1] =2;
+
+        Experem exp = new Experem("dsdadf");
+    }
+}
---
+++ src/golovach/Experem.java
@@ -0,0 +1,17 @@
+package golovach;
+
+public class Experem <T>{
+    private T t;
+
+    public Experem(T t) {
+        this.t = t;
+    }
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+}
